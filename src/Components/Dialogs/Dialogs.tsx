088939e52b596a5b1e9179e890f16d9f91f43dc5
Ivan0195 <iihnatsenka95@gmail.com>
2022-01-31 16:57:07 +0300

    24lesson

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -20,26 +20,48 @@ export const DialogItem = (props: DialogItemPropsType) => {
     </div>
 }
 
+
+
 export const Message = (props:MessagePropsType)=> {
   return  <div className={classes.message}>{props.message}</div>
 }
 
 
 export const Dialogs = () => {
+
+    const users = [
+        {id:'1', userName:'Viktor Tsoy'},
+        {id:'2', userName:'Corey Taylor'},
+        {id:'3', userName:'Jared Leto'},
+        {id:'4', userName:'Hoy'},
+        {id:'5', userName:'Misha Gorshnev'},
+        {id:'6', userName:'Tim McIlrath'}
+    ]
+
+    const messages = [
+        {id:'1', message:'Answer your phone'},
+        {id:'2', message:'Add me to your friend =)'},
+        {id:'3', message:'Do you remember anything from the last party?'},
+    ]
+
+
+
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
-                <DialogItem id={'1'} userName={'Viktor Tsoy'}/>
+                {users.map(m=>(<DialogItem userName={m.userName} id={m.id}/>))}
+              {/*  <DialogItem id={'1'} userName={'Viktor Tsoy'}/>
                 <DialogItem id={'2'} userName={'Corey Taylor'}/>
                 <DialogItem id={'3'} userName={'Jared Leto'}/>
                 <DialogItem id={'4'} userName={'Hoy'}/>
                 <DialogItem id={'5'} userName={'Misha Gorshnev'}/>
-                <DialogItem id={'6'} userName={'Tim McIlrath'}/>
+                <DialogItem id={'6'} userName={'Tim McIlrath'}/>*/}
             </div>
             <div className={classes.messages}>
-                <Message message={'Answer your phone'}/>
+                {messages.map(m=><Message message={m.message}/>)}
+              {/*  <Message message={'Answer your phone'}/>
                 <Message message={'Add me to your friend =)'}/>
-                <Message message={'Do you remember anything from the last party?'}/>
+                <Message message={'Do you remember anything from the last party?'}/>*/}
             </div>
         </div>
     );
--- src/Components/ProfileComponent/MyPosts/MyPosts.tsx
+++ src/Components/ProfileComponent/MyPosts/MyPosts.tsx
@@ -3,6 +3,46 @@ import classes from './MyPosts.module.css'
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+
+    const posts = [
+        {
+            postText: 'Hello! I miss you!',
+            avaAdress: 'https://b1.culture.ru/c/735787.jpg',
+            userName: 'Viktor Tsoy', likesCount: 22
+        },
+        {
+            postText: "Hey, let's go fishing next weekend ",
+            avaAdress: 'https://salvemusic.com.ua/wp-content/uploads/2020/10/corey-taylor-1024x576.jpg',
+            userName: 'Corey Taylor',
+            likesCount: 15
+        },
+        {
+            postText: 'How is your React progress?',
+            avaAdress: 'https://fs.kinomania.ru/file/person/c/ec/cec6f62cfb44b1be110b7bf70c8362d8.jpeg',
+            userName: 'Jared Leto',
+            likesCount: 25
+        },
+        {
+            postText: 'Nice photos =) ',
+            avaAdress: 'https://peterburg.center/sites/default/files/img/event_m/2020-02/3ealj3jer9q.jpg',
+            userName: 'Hoy',
+            likesCount: 50
+        },
+        {
+            postText: 'Happy Birthday!',
+            avaAdress: 'https://m.the-flow.ru/uploads/images/resize/830x0/adaptiveResize/13/97/19/48/63/425ad4140cd7.png',
+            userName: 'Misha Gorshnev',
+            likesCount: 44
+        },
+        {
+            postText: 'Happy NewYear',
+            avaAdress: 'https://i.pinimg.com/originals/b6/ab/43/b6ab431f8eaddd29b49f6a1ad8b5effc.jpg',
+            userName: 'Tim McIlrath',
+            likesCount: 33
+        }
+    ]
+
+
     return (
         <div>
             <div className={classes.myPostsTitle}>
@@ -17,24 +57,10 @@ const MyPosts = () => {
                 </div>
             </div>
             <div className={classes.posts}>
-                <Post postText='Hello! I miss you!' avaAddress={'https://b1.culture.ru/c/735787.jpg'}
-                      userName={'Viktor Tsoy'} likesCount={22}/>
-                <Post postText="Hey, let's go fishing next weekend "
-                      avaAddress={'https://salvemusic.com.ua/wp-content/uploads/2020/10/corey-taylor-1024x576.jpg'}
-                      userName={'Corey Taylor'} likesCount={15}/>
-                <Post postText='How is your React progress?'
-                      avaAddress={'https://fs.kinomania.ru/file/person/c/ec/cec6f62cfb44b1be110b7bf70c8362d8.jpeg'}
-                      userName={'Jared Leto'} likesCount={25}/>
-                <Post postText='Nice photos =) '
-                      avaAddress={'https://peterburg.center/sites/default/files/img/event_m/2020-02/3ealj3jer9q.jpg'}
-                      userName={'Hoy'} likesCount={50}/>
-                <Post postText='Happy Birthday!'
-                      avaAddress={'https://m.the-flow.ru/uploads/images/resize/830x0/adaptiveResize/13/97/19/48/63/425ad4140cd7.png'}
-                      userName={'Misha Gorshnev'} likesCount={44}/>
-                <Post postText='Happy NewYear'
-                      avaAddress={'https://i.pinimg.com/originals/b6/ab/43/b6ab431f8eaddd29b49f6a1ad8b5effc.jpg'}
-                      userName={'Tim McIlrath'} likesCount={33}/>
-
+                {posts.map(m => <Post avaAddress={m.avaAdress}
+                                      postText={m.postText}
+                                      userName={m.userName}
+                                      likesCount={m.likesCount}/>)}
             </div>
         </div>
     );
